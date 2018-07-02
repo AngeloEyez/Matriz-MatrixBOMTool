bb3971c726a25315e2c94c0270e24e41e453ab38
Angeloeyez <gavenhuang@gmail.com>
2018-07-02 16:40:13 +0800

    Change dialog to fix size

--- Matriz/Form1.Designer.cs
+++ Matriz/Form1.Designer.cs
@@ -155,6 +155,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
